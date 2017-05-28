f774646812823bc37350d1850ff9199381e99b3a
Mark Bello <mcbello51286@gmail.com>
2017-05-28 14:45:51 -0400

    Re-try fixing unexpected token in Venues route

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
-app.get("venues", function(req, res) {
+app.get("/venues", function(req, res) {
     var venues = [
-        { name: "Webster Hall", image: "http://www.websterhall.com/redesign/wp-content/uploads/mdicon.jpg" } { name: "Playstation Theater", image: "http://mattybraps.com/wp-content/uploads/2016/03/playstation-theater.jpg" } { name: "Atlantic Avenue Warehouse", image: "http://www.thenocturnaltimes.com/wp-content/uploads/2017/04/anjuna-nyc-2.jpg" }
-    ]
+        { name: "Webster Hall", image: "http://www.websterhall.com/redesign/wp-content/uploads/mdicon.jpg" }, { name: "Playstation Theater", image: "http://mattybraps.com/wp-content/uploads/2016/03/playstation-theater.jpg" }, { name: "Atlantic Avenue Warehouse", image: "http://www.thenocturnaltimes.com/wp-content/uploads/2017/04/anjuna-nyc-2.jpg" }
+    ];
 
     res.render("venues");
 });
