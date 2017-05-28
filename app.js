177bdf60a19a51db16d753f5a36b81de5b6033ca
Mark Bello <mcbello51286@gmail.com>
2017-05-28 14:39:16 -0400

    Add Venues view

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+app.get("venues"),
+    function(req, res) {
+        var venues = [
+            { name: "Webster Hall", image: "http://www.websterhall.com/redesign/wp-content/uploads/mdicon.jpg" } { name: "Playstation Theater", image: "http://mattybraps.com/wp-content/uploads/2016/03/playstation-theater.jpg" } { name: "Atlantic Avenue Warehouse", image: "http://www.thenocturnaltimes.com/wp-content/uploads/2017/04/anjuna-nyc-2.jpg" }
+        ]
+
+        res.render("venues");
+    }
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("PLURview has started!");
 });
